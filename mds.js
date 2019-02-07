426dc325b68bed7555166b55ee7e4fcde03460cc
Rickard1983 <43532689+Rickard1983@users.noreply.github.com>
2019-02-07 23:34:49 +0100

    Add files via upload

--- mds.js
+++ mds.js
@@ -1,7 +1,39 @@
+function disable(){
+let text = document.querySelectorAll("input")
+for (let all of text){
+  all.setAttribute("disabled", true);
+  console.log(all);
 
-let mds = {
-  textField: function(element){},
-  switch: function(element){},
-  checkbox: function(element){},
-  radioButton: function(element){},
+
+
+  }
+  let labels = document.querySelectorAll('label');
+    for(let label of labels) {
+    }
+
 }
+
+function enable(){
+let text = document.querySelectorAll("input")
+for (let all of text){
+  all.removeAttribute("disabled");
+  console.log(all);
+
+  }
+  let labels = document.querySelectorAll('label');
+    for(let label of labels) {
+    }
+}
+
+
+
+let button = document.querySelector('.mds-disable__button')
+button.addEventListener('click', disable)
+
+let buttonenable = document.querySelector('.mds-enable__button')
+buttonenable.addEventListener('click', enable)
+
+textfield();
+checkbox();
+radiobutton();
+mds-switch();
